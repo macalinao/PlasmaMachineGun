77cac9299e5f364ddeff8b9d058fac7530b0fc28
Ian Macali <ianmacalinao@gmail.com>
2012-03-16 01:24:08 -0500

    Added main plugin class.

---
+++ src/main/java/net/celestiusmc/plasmamachinegun/PlasmaMachineGun.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.celestiusmc.plasmamachinegun;
+
+import java.util.logging.Level;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * PlasmaMachineGun main class.
+ */
+public class PlasmaMachineGun extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "Plugin disabled.");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.INFO, "Plugin enabled.");
+    }
+    
+}
